9705e2eab86d4aabaead8adb87d69410fc0fc65a
Pierre Guilhou <pierre.guilhou14@gmail.com>
2017-10-07 17:17:43 +0200

    Change deploy remote

---
+++ bundle.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],[function(e,o){"use strict";console.log("Hello fellow hacker. Feel free to reach out at %cpierre@spendesk.com","font-style: italic; text-decoration: underline; color: #33CCCC;")}]);
+//# sourceMappingURL=bundle.js.map
