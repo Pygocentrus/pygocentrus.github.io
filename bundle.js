9eba5a4cbc3237a7d3d0c119944d4e002b9f2cda
Pierre Guilhou <pierre.guilhou14@gmail.com>
2017-10-07 20:17:35 +0200

    Add secondary nav

--- bundle.js
+++ bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([2],[function(e,o){"use strict";console.log("Hello fellow hacker! Feel free to reach out at %cpierre@spendesk.com","font-style: italic; text-decoration: underline; color: #33CCCC;")}]);
+webpackJsonp([2],[function(e,o){"use strict";console.log("Hello fellow hacker! Feel free to reach out at %cpierre@spendesk.com","font-style: italic; text-decoration: underline; color: #FA8072;")}]);
 //# sourceMappingURL=bundle.js.map
